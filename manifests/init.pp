74665838247c787ecafbbcc60e96f63beaed025b
Steve DiDomenico <steve@northwestern.edu>
2016-04-18 11:21:26 -0500

    Updated for Fedora 4.5.1 RC3

--- manifests/init.pp
+++ manifests/init.pp
@@ -53,16 +53,16 @@ case $storagetype {
       fcrepo_configdir    => '/fedora/config',
       java_homedir        => '/usr/lib/jvm/jre-1.8.0',
       # These three lines are specific to the MySQL config:
-      fcrepo_repository_json => 'https://raw.githubusercontent.com/fcrepo4/fcrepo4/fcrepo-4.5.1-RC-2/fcrepo-configs/src/main/resources/config/minimal-default/repository.json',
-      fcrepo_infinispan_xml  => 'https://raw.githubusercontent.com/fcrepo4/fcrepo4/fcrepo-4.5.1-RC-2/fcrepo-configs/src/main/resources/config/infinispan/jdbc-mysql/infinispan.xml',
+      fcrepo_repository_json => 'https://raw.githubusercontent.com/fcrepo4/fcrepo4/fcrepo-4.5.1-RC-3/fcrepo-configs/src/main/resources/config/minimal-default/repository.json',
+      fcrepo_infinispan_xml  => 'https://raw.githubusercontent.com/fcrepo4/fcrepo4/fcrepo-4.5.1-RC-3/fcrepo-configs/src/main/resources/config/infinispan/jdbc-mysql/infinispan.xml',
       fcrepo_db_port      => '3306',
       #
-      fcrepo_jgroups_fcrepo_tcp_xml => 'https://raw.githubusercontent.com/fcrepo4/fcrepo4/fcrepo-4.5.1-RC-2/fcrepo-configs/src/main/resources/config/jgroups-fcrepo-tcp.xml',
+      fcrepo_jgroups_fcrepo_tcp_xml => 'https://raw.githubusercontent.com/fcrepo4/fcrepo4/fcrepo-4.5.1-RC-3/fcrepo-configs/src/main/resources/config/jgroups-fcrepo-tcp.xml',
       fcrepo_db_host      => 'localhost',
       fcrepo_db_username  => 'fcrepouser',
       fcrepo_db_password  => '34inasdfioHDSFIKSHio',
       tomcat_source       => 'http://archive.apache.org/dist/tomcat/tomcat-7/v7.0.68/bin/apache-tomcat-7.0.68.tar.gz',
-      fcrepo_warsource    => 'https://github.com/fcrepo4/fcrepo4/releases/download/fcrepo-4.5.1-RC-2/fcrepo-webapp-4.5.1-RC-2.war',
+      fcrepo_warsource    => 'https://github.com/fcrepo4/fcrepo4/releases/download/fcrepo-4.5.1-RC-3/fcrepo-webapp-4.5.1-RC-3.war',
     }->
     fcrepo::service { 'tomcat-fcrepo':
       service_enable      => true,
@@ -94,16 +94,16 @@ case $storagetype {
       fcrepo_configdir    => '/fedora/config',
       java_homedir        => '/usr/lib/jvm/jre-1.8.0',
       # These three lines are needed for the PostgreSQL config:
-      fcrepo_repository_json => 'https://raw.githubusercontent.com/fcrepo4/fcrepo4/fcrepo-4.5.1-RC-2/fcrepo-configs/src/main/resources/config/jdbc-postgresql/repository.json',
-      fcrepo_infinispan_xml  => 'https://raw.githubusercontent.com/fcrepo4/fcrepo4/fcrepo-4.5.1-RC-2/fcrepo-configs/src/main/resources/config/infinispan/jdbc-postgresql/infinispan.xml',
+      fcrepo_repository_json => 'https://raw.githubusercontent.com/fcrepo4/fcrepo4/fcrepo-4.5.1-RC-3/fcrepo-configs/src/main/resources/config/jdbc-postgresql/repository.json',
+      fcrepo_infinispan_xml  => 'https://raw.githubusercontent.com/fcrepo4/fcrepo4/fcrepo-4.5.1-RC-3/fcrepo-configs/src/main/resources/config/infinispan/jdbc-postgresql/infinispan.xml',
       fcrepo_db_port      => '5432',
       #
-      fcrepo_jgroups_fcrepo_tcp_xml => 'https://raw.githubusercontent.com/fcrepo4/fcrepo4/fcrepo-4.5.1-RC-2/fcrepo-configs/src/main/resources/config/jgroups-fcrepo-tcp.xml',
+      fcrepo_jgroups_fcrepo_tcp_xml => 'https://raw.githubusercontent.com/fcrepo4/fcrepo4/fcrepo-4.5.1-RC-3/fcrepo-configs/src/main/resources/config/jgroups-fcrepo-tcp.xml',
       fcrepo_db_host      => 'localhost',
       fcrepo_db_username  => 'fcrepouser',
       fcrepo_db_password  => 'akldsnf34SDKNDSoiwneiov',
       tomcat_source       => 'http://archive.apache.org/dist/tomcat/tomcat-7/v7.0.68/bin/apache-tomcat-7.0.68.tar.gz',
-      fcrepo_warsource    => 'https://github.com/fcrepo4/fcrepo4/releases/download/fcrepo-4.5.1-RC-2/fcrepo-webapp-4.5.1-RC-2.war',
+      fcrepo_warsource    => 'https://github.com/fcrepo4/fcrepo4/releases/download/fcrepo-4.5.1-RC-3/fcrepo-webapp-4.5.1-RC-3.war',
     }->
     fcrepo::service { 'tomcat-fcrepo':
       service_enable      => true,
@@ -124,12 +124,12 @@ case $storagetype {
       fcrepo_configdir    => '/fedora/config',
       java_homedir        => '/usr/lib/jvm/jre-1.8.0',
       # These two lines are specific to the LevelDB config:
-      fcrepo_repository_json => 'https://raw.githubusercontent.com/fcrepo4/fcrepo4/fcrepo-4.5.1-RC-2/fcrepo-configs/src/main/resources/config/minimal-default/repository.json',
-      fcrepo_infinispan_xml  => 'https://raw.githubusercontent.com/fcrepo4/fcrepo4/fcrepo-4.5.1-RC-2/fcrepo-configs/src/main/resources/config/infinispan/leveldb-default/infinispan.xml',
+      fcrepo_repository_json => 'https://raw.githubusercontent.com/fcrepo4/fcrepo4/fcrepo-4.5.1-RC-3/fcrepo-configs/src/main/resources/config/minimal-default/repository.json',
+      fcrepo_infinispan_xml  => 'https://raw.githubusercontent.com/fcrepo4/fcrepo4/fcrepo-4.5.1-RC-3/fcrepo-configs/src/main/resources/config/infinispan/leveldb-default/infinispan.xml',
       #
-      fcrepo_jgroups_fcrepo_tcp_xml => 'https://raw.githubusercontent.com/fcrepo4/fcrepo4/fcrepo-4.5.1-RC-2/fcrepo-configs/src/main/resources/config/jgroups-fcrepo-tcp.xml',
+      fcrepo_jgroups_fcrepo_tcp_xml => 'https://raw.githubusercontent.com/fcrepo4/fcrepo4/fcrepo-4.5.1-RC-3/fcrepo-configs/src/main/resources/config/jgroups-fcrepo-tcp.xml',
       tomcat_source       => 'http://archive.apache.org/dist/tomcat/tomcat-7/v7.0.68/bin/apache-tomcat-7.0.68.tar.gz',
-      fcrepo_warsource    => 'https://github.com/fcrepo4/fcrepo4/releases/download/fcrepo-4.5.1-RC-2/fcrepo-webapp-4.5.1-RC-2.war',
+      fcrepo_warsource    => 'https://github.com/fcrepo4/fcrepo4/releases/download/fcrepo-4.5.1-RC-3/fcrepo-webapp-4.5.1-RC-3.war',
     }->
     fcrepo::service { 'tomcat-fcrepo':
       service_enable      => true,
